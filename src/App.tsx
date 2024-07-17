a14bacce8d5c45584161a3693befabac86154784
nemcabong <nemcabong@gmail.com>
2024-07-17 23:14:34 +0700

    Them trang signup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { ThemeProvider } from "@/components/theme-provider";
-import { Button } from "./components/ui/button";
+// import { Button } from "./components/ui/button";
 import { LoginForm } from "./pages/LogIn";
+import { Route, Routes } from "react-router-dom";
+import { SignUpForm } from "./pages/SignUp";
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <LoginForm />
+    <ThemeProvider defaultTheme="dark">
+      <Routes>
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/signup" element={<SignUpForm />} />
+      </Routes>
     </ThemeProvider>
   );
 }
