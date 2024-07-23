b95a304387f7da2803b65b6ebe9d3ff6e516b485
nemcabong <nemcabong@gmail.com>
2024-07-23 15:48:54 +0700

    Lam trang classroom

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,17 @@ import { LoginForm } from "./pages/Login/LogIn.tsx";
 import { SignUpForm } from "./pages/SignUp/SignUp.tsx";
 import { Dashboard } from "./pages/Dashboard/Dashboard.tsx";
 import { TaskBoard } from "./pages/TaskBoard/TaskBoard.tsx";
+import { ClassroomDetail } from "./pages/ClassroomDetail/ClassroomDetail.tsx";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark">
       <Routes>
+        <Route path="/" element={<Dashboard />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/signup" element={<SignUpForm />} />
-        <Route path="/" element={<Dashboard />} />
         <Route path="/tasks" element={<TaskBoard />} />
+        <Route path="classroom" element={<ClassroomDetail />} />
         <Route path="*" element={<h1>Not Found</h1>} />
       </Routes>
     </ThemeProvider>
