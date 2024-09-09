d105ae75a42c35a6fdf1c8f17c01a349b9e63b82
Nguyen Minh Hoang <nemcabong@gmail.com>
2024-09-09 22:16:13 +0700

    Xoa dependencies khong can thiet

--- src/pages/admin/AdminDashboard/components/PieChart.tsx
+++ src/pages/admin/AdminDashboard/components/PieChart.tsx
@@ -1,10 +1,8 @@
-import { TrendingUp } from "lucide-react";
 import { LabelList, Pie, PieChart, ResponsiveContainer } from "recharts";
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
@@ -83,48 +81,6 @@ export function PieChartDashboard() {
           </ResponsiveContainer>
         </ChartContainer>
       </CardContent>
-    </Card>
-  );
-}
-
-export function Component() {
-  return (
-    <Card className="flex flex-col">
-      <CardHeader className="items-center pb-0">
-        <CardTitle>Pie Chart - Label List</CardTitle>
-        <CardDescription>January - June 2024</CardDescription>
-      </CardHeader>
-      <CardContent className="flex-1 pb-0">
-        <ChartContainer
-          config={chartConfig}
-          className="mx-auto aspect-square max-h-[250px]"
-        >
-          <PieChart>
-            <ChartTooltip
-              content={<ChartTooltipContent nameKey="visitors" hideLabel />}
-            />
-            <Pie data={chartData} dataKey="visitors">
-              <LabelList
-                dataKey="browser"
-                className="fill-background"
-                stroke="none"
-                fontSize={12}
-                formatter={(value: keyof typeof chartConfig) =>
-                  chartConfig[value]?.label
-                }
-              />
-            </Pie>
-          </PieChart>
-        </ChartContainer>
-      </CardContent>
-      <CardFooter className="flex-col gap-2 text-sm">
-        <div className="flex items-center gap-2 font-medium leading-none">
-          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
-        </div>
-        <div className="leading-none text-muted-foreground">
-          Showing total visitors for the last 6 months
-        </div>
-      </CardFooter>
     </Card>
   );
 }
