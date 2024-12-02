e5e8d8818f345acfd0f166efe0ee9d2c544161fe
Nguyen Minh Hoang <nemcabong@gmail.com>
2024-12-02 10:33:47 +0700

    Cap nhat

--- src/pages/client/CodingExercise/components/DescriptionDetailTab.tsx
+++ src/pages/client/CodingExercise/components/DescriptionDetailTab.tsx
@@ -8,6 +8,7 @@ import {
 import MDEditor from "@uiw/react-md-editor";
 import { Badge } from "@/components/ui/badge";
 import { format } from "date-fns";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export default function DescriptionDetailTab({
   title,
@@ -21,35 +22,37 @@ export default function DescriptionDetailTab({
   submittedAt?: string;
 }) {
   return (
-    <Card className="border-none">
-      <CardHeader>
-        <div className="flex justify-between items-center">
-          <CardTitle>
-            {title}
-            <Badge
-              className="ml-6"
-              variant={
-                userStatus === "not-done"
-                  ? "destructive"
-                  : userStatus === "graded"
-                  ? "default"
-                  : "secondary"
-              }
-            >
-              {userStatus}
-            </Badge>
-          </CardTitle>
-          {submittedAt && (
-            <span className="text-sm text-gray-500">
-              Submitted At: {format(new Date(submittedAt), "PPpp")}
-            </span>
-          )}
-        </div>
-      </CardHeader>
-      <CardContent>
-        <MDEditor.Markdown source={markdownContent} className="text-sm" />
-      </CardContent>
-      <CardFooter></CardFooter>
-    </Card>
+    <ScrollArea className="w-[81vh]">
+      <Card className="border-none">
+        <CardHeader>
+          <div className="flex justify-between items-center">
+            <CardTitle>
+              {title}
+              <Badge
+                className="ml-6"
+                variant={
+                  userStatus === "not-done"
+                    ? "destructive"
+                    : userStatus === "graded"
+                    ? "default"
+                    : "secondary"
+                }
+              >
+                {userStatus}
+              </Badge>
+            </CardTitle>
+            {submittedAt && (
+              <span className="text-sm text-gray-500">
+                Submitted At: {format(new Date(submittedAt), "PPpp")}
+              </span>
+            )}
+          </div>
+        </CardHeader>
+        <CardContent>
+          <MDEditor.Markdown source={markdownContent} className="text-sm" />
+        </CardContent>
+        <CardFooter></CardFooter>
+      </Card>
+    </ScrollArea>
   );
 }
